f1f0c9db673f7a6e26b6eff273ce098478dc51b8
pradipk1 <pradipshukla12699@gmail.com>
2024-03-16 12:30:55 +0530

    day02 assignment started

---
+++ Day02-(Directives)/angular-directives/src/app/toggle-highlight.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ToggleHighlightDirective } from './toggle-highlight.directive';
+
+describe('ToggleHighlightDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ToggleHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
